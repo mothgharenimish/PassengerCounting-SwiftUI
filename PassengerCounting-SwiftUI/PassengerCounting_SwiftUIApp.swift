fe3c47a36794f882d7e09d00e7fe7c5c2679b52d
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-11 17:39:05 +0530

    Initial Commit

---
+++ PassengerCounting-SwiftUI/PassengerCounting_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PassengerCounting_SwiftUIApp.swift
+//  PassengerCounting-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PassengerCounting_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PassengerCounting-SwiftUITests/PassengerCounting_SwiftUITests.swift
@@ -0,0 +1,17 @@
+//
+//  PassengerCounting_SwiftUITests.swift
+//  PassengerCounting-SwiftUITests
+//
+//  Created by Nimish Sharad Mothghare on 11/03/25.
+//
+
+import Testing
+@testable import PassengerCounting_SwiftUI
+
+struct PassengerCounting_SwiftUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
