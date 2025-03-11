5e22ab377574d1f2b42b5be31eafcdf945105623
mothgharenimish <mothgharenimish0@gmail.com>
2025-03-11 21:06:26 +0530

    Xcode Update Changes

---
+++ PassengerCounting-SwiftUI/Views/PassangerButton.swift
@@ -0,0 +1,18 @@
+//
+//  PassangerButton.swift
+//  PassengerCounting-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 11/03/25.
+//
+
+import SwiftUI
+
+struct PassangerButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PassangerButton()
+}
---
+++ PassengerCounting-SwiftUI/Views/RoundedButton.swift
@@ -0,0 +1,18 @@
+//
+//  RoundedButton.swift
+//  PassengerCounting-SwiftUI
+//
+//  Created by Nimish Sharad Mothghare on 11/03/25.
+//
+
+import SwiftUI
+
+struct RoundedButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoundedButton()
+}
